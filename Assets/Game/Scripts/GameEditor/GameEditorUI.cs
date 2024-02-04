39079c9e2ccc588ef8786c89bff9eabe062f965d
warzes <warzes@mail.ru>
2024-02-05 02:05:43 +1000

    - наброски редактора

---
+++ Assets/Game/Scripts/GameEditor/GameEditorUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameEditorUI : MonoBehaviour
+{
+	private void Start()
+	{
+	}
+	private void Update()
+	{
+	}
+
+	public void ButtonTest()
+	{
+		Debug.Log("Test");
+	}
+}
